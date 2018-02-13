429240067597dd92cedef3d3b016ee64af28f10c
Tyryk <anyatirik@gmail.com>
2018-02-13 18:53:19 +0200

    changeStyle

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("Hello anna");
+angular.module("exampleApp", []).controller("taskCtrl", function () {
+	var vm = this;
+	vm.style = [
+		{
+			header: "headerColor1",
+			box1: "box1Color",
+			box2: "box2Color",
+			box3: "box3Color"
+			
+		},
+		{
+			header: "headerColor2",
+			box1: "box1_2Color",
+			box2: "box2_2Color",
+			box3: "box3_2Color"
+			
+		},
+		{
+			header: "headerColor3",
+			box1: "box1_3Color",
+			box2: "box2_3Color",
+			box3: "box3_3Color"
+			
+		}
+	];
+	vm.changeStyle = vm.style[0];
+	vm.change1 = function(){
+		vm.changeStyle = vm.style[0];
+		console.log(vm.changeStyle);
+	};
+	vm.change2 = function(){
+		vm.changeStyle = vm.style[1];
+		console.log(vm.changeStyle);
+	};
+	vm.change3 = function(){
+		vm.changeStyle = vm.style[2];
+		console.log(vm.changeStyle);
+	};
+})
